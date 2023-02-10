303d191f01f5674e1d3a906faf45b21a03c9f758
zawar6006 <120322150+zawar6006@users.noreply.github.com>
2023-02-11 00:47:21 +0700

    second  commit

---
+++ CoursesView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="background-color: lightgray;">
+    <h3 style="padding: 10px;margin: 10px 0;">รายวิชาทั้งหมด</h3>
+    <hr style="margin: 10px 0;" />
+    <table style="width: 100%; border-collapse: collapse;">
+      <tr style="background-color: lightblue;">
+        <th style="padding: 10px; text-align: left">รหัสวิชา</th>
+        <th style="padding: 10px; text-align: left">ชื่อวิชา</th>
+      </tr>
+
+      <tr v-for="(course, index) in courses" :key="index" style="background-color: white;">
+    <td style="padding: 10px; text-align: left">{{ course.course_id }}</td>
+    <td style="padding: 10px; text-align: left">
+      {{ course.course_name }}
+    </td>
+  </tr>
+</table>
+</div>
+</template>
+<script setup>
+import courses from "../json/cs_courses.json";
+</script>
+<style></style>
---
+++ HomeView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import news from "../json/news.json";
+</script>
+
+<template>
+  <div class="content" style="background-color: lightgray">
+    <div style="padding: 1.5em;">
+      <h1 class="m-0" style="color: black">
+        ข่าวสารล่าสุด
+      </h1>
+      <h3 class="m-0 color-gray" style="color: black">สาขาวิชาวิทยาการคอมพิวเตอร์</h3>
+    </div>
+    <hr />
+    <article>
+  <table style="width: 100%;">
+    <tr>
+      <th style="text-align: left; color: black; padding: 0.5em;">วันที่</th>
+      <th style="text-align: left; color: black; padding: 0.5em;">หัวข้อข่าว</th>
+    </tr>
+
+    <tr v-for="news_content in news" :key="news_content.newsId">
+      <td style="text-align: left; color: black; padding: 0.5em;">{{ news_content.News_Date }}</td>
+      <td style="text-align: left; color: black; padding: 0.5em;">
+        {{ news_content.News_Title }}
+      </td>
+    </tr>
+  </table>
+</article>
+
+  </div>
+</template>
+<style scoped>
+.content {
+  margin-top: 1em;
+  margin-bottom: 1em;
+  padding-bottom: 2em;
+  padding-left: 2em;
+}
+ma .m-0 {
+  margin: unset;
+}
+</style>
